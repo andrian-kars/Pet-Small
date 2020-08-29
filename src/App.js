ea45bcce8705b9512cdc90868fb881ed8291ba9f
pmAndrian <66482484+pmAndrian@users.noreply.github.com>
2020-08-29 22:22:28 +0300

    halfway-throw
    Changed class compnent to func and add languages

--- src/App.js
+++ src/App.js
@@ -1,81 +1,72 @@
 import React from 'react'
 import './App.scss'
 import ListItem from './ListItem/ListItem'
+import { I18nProvider, LOCALES } from './i18n'
+import translate from './i18n/translate'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      items: [],
-      currentItem: {
-        text: '',
-        key: ''
-      }
-    }
-    this.handleInput = this.handleInput.bind(this)
-    this.addItem = this.addItem.bind(this)
-    this.deleteItem = this.deleteItem.bind(this)
-    this.setUpdate = this.setUpdate.bind(this)
-  }
-  handleInput(e) {
-    this.setState({
-      currentItem: {
-        text: e.target.value,
-        key: Date.now()
-      }
+function App(props) {
+  const [items, setItems] = React.useState([])
+  const [currentItem, setCurrentItem] = React.useState({
+    text: '',
+    key: ''
+  })
+
+  let handleInput = (e) => {
+    setCurrentItem({
+      text: e.target.value,
+      key: Date.now()
     })
   }
-  addItem(e) {
+  let addItem = (e) => {
     e.preventDefault()
-    const newItem = this.state.currentItem
+    const newItem = currentItem
     if (newItem.text !== "") {
-      const newItems = [...this.state.items, newItem]
-      this.setState({
-        items: newItems,
-        currentItem: {
-          text: '',
-          key: ''
-        }
+      const newItems = [...items, newItem]
+
+      setItems(newItems)
+      setCurrentItem({
+        text: '',
+        key: ''
       })
     }
   }
-  deleteItem(key) {
-    const filteredItems = this.state.items.filter(item =>
+  let deleteItem = (key) => {
+    const filteredItems = items.filter(item =>
       item.key !== key)
-    this.setState({
-      items: filteredItems
-    })
+    setItems(filteredItems)
   }
-  setUpdate(text, key) {
-    const items = this.state.items
+  let setUpdate = (text, key) => {
     items.map(item => {
       if (item.key === key) {
         item.text = text
       }
       return item.text
     })
-    this.setState({
-      items: items
-    })
+    setItems(items)
   }
-  render() {
-    return (
+  let [locale, setLocale] = React.useState(LOCALES.ENGLISH)
+  return (
+    <I18nProvider locale={locale}>
       <div className="App">
-        <h1 className="heading">Application for management</h1>
+        <h1 className="heading">{translate("heading")}</h1>
         <div className="whrapper">
           <header className="header">
-            <form id="todo-form" className="form" onSubmit={this.addItem}>
-              <input className="enter-task" type="text" placeholder="Ender task" value={this.state.currentItem.text} onChange={this.handleInput} />
-              <button className="add" type="submit">Add</button>
+            <form id="todo-form" className="form" onSubmit={addItem}>
+              <input className="enter-task" type="text" placeholder="..." value={currentItem.text} onChange={handleInput} />
+              <button className="add" type="submit">{translate("add")}</button>
             </form>
           </header>
           <main className="main">
-            <ListItem items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate} />
+            <ListItem items={items} deleteItem={deleteItem} setUpdate={setUpdate} />
           </main>
+          <footer className="footer">
+            <div className="translate english" onClick={() => setLocale(LOCALES.ENGLISH)}></div>
+            <div className="translate russian" onClick={() => setLocale(LOCALES.RUSSIAN)}></div>
+          </footer>
         </div>
       </div>
-    )
-  }
+    </I18nProvider>
+  )
 }
 
 export default App
---
+++ src/i18n/messages/ru-RU.js
@@ -0,0 +1,9 @@
+import { LOCALES } from '../locales'
+
+export default {
+    [LOCALES.RUSSIAN]: {
+        'heading': 'Приложения для менеджмента',
+        'enter': 'Введите задачу',
+        'add': 'Доб.'
+    }
+}
