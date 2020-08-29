f9b937a09d5fe1b49248b81529a5c8ffcf87f2fd
pmAndrian <66482484+pmAndrian@users.noreply.github.com>
2020-08-29 22:52:00 +0300

    Added-three-more-lang

--- src/i18n/messages/index.js
+++ src/i18n/messages/index.js
@@ -1,7 +1,13 @@
 import en from './en-US'
+import jp from './jp-JP'
+import de from './de-DE'
+import ua from './ua-UA'
 import ru from './ru-RU'
 
 export default {
     ...en,
+    ...jp,
+    ...de,
+    ...ua,
     ...ru
 }
